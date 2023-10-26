32d9e07072b9e8df4326fd047985c8974ec09f5b
Hy <nguyengiahy2911@gmail.com>
2023-10-26 11:27:35 +1100

    1st commit

--- dataset.py
+++ dataset.py
@@ -13,9 +13,9 @@ class BilingualDataset(Dataset):
         self.seq_len = seq_len
         
         # Convert special tokens into token_id
-        self.sos_token = torch.Tensor([tokenizer_src.token_to_id(['SOS'])], dtype = torch.int64)
-        self.eos_token = torch.Tensor([tokenizer_src.token_to_id(['EOS'])], dtype = torch.int64)
-        self.pad_token = torch.Tensor([tokenizer_src.token_to_id(['PAD'])], dtype = torch.int64)
+        self.sos_token = torch.tensor([tokenizer_src.token_to_id('[SOS]')], dtype = torch.int64)
+        self.eos_token = torch.tensor([tokenizer_src.token_to_id('[EOS]')], dtype = torch.int64)
+        self.pad_token = torch.tensor([tokenizer_src.token_to_id('[PAD]')], dtype = torch.int64)
     
     def __len__(self):
         return len(self.ds)
--- test.py
+++ test.py
@@ -1,8 +1,49 @@
-import torch
-import torch.nn as nn
 from datasets import load_dataset
 from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
 from tokenizers.trainers import WordLevelTrainer        # the class that can create the vocab given the list of sentences
-from tokenizers.pre_tokenizers import Whitespace 
+from tokenizers.pre_tokenizers import Whitespace
+
 from pathlib import Path
+
+def get_all_sentences(dataset, language):
+    for item in dataset:
+        yield item['translation'][language]
+
+
+def get_or_build_tokenizer(config, dataset, language):
+    tokenizer_path = Path(config['tokenizer_file'].format(language))    # e.g: config[tokenizer_file] = ".../tokenizers/tokenizer_{lang}.json"
+    if not Path.exists(tokenizer_path):
+        tokenizer = Tokenizer(WordLevel(unk_token = '[UNK]'))           # If the tokenizer sees a word that is not in the vocabulary it was trained on, it will replace the word with [UNK]
+        trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2)        # min_freq: for a word to appear in the vocab, it needs to appear at least 2 times
+        tokenizer.train_from_iterator(get_all_sentences(dataset, language), trainer=trainer)
+        tokenizer.save(str(tokenizer_path))
+    else:
+        tokenizer = Tokenizer.from_file(str(tokenizer_path))
+
+    return tokenizer
+
+
+def get_config():
+    return {
+        "batch_size": 8,
+        "num_epochs": 1,
+        "lr": 10**-4,
+        "seq_len": 350,
+        "d_model": 512,
+        "lang_src": "en",
+        "lang_tgt": "it",
+        "model_folder": "weights",
+        "model_basename": "tmodel_",
+        "preload": None,
+        "tokenizer_file": "tokenizer_{0}.json",
+        "experiment_name": "runs/tmodel"
+    }
+
+config = get_config()
+ds_raw = load_dataset('opus_books', f'{config["lang_src"]}-{config["lang_tgt"]}', split="train")
+
+tokenizer_src = get_or_build_tokenizer(config, ds_raw, config["lang_src"])
+
+sos_id = tokenizer_src.token_to_id('[SOS]')
+print(sos_id)
